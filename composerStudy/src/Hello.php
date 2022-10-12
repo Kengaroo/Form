3499ea0f6f0a03df4618e0564327692ca0702303
Natalia <nataliya_nedbaylo@yahoo.com>
2022-10-12 10:55:18 +0200

    composer autoload testing;

---
+++ composerStudy/src/Hello.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Hello
+{
+    public function __construct()
+    {
+
+    }
+
+    static public function talk() :string
+    {
+        return "Hello World";
+    }
+}
